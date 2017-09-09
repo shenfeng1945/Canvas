2c822f3830f134dbeeda6b7877a7bfc4c97b7dfd
shenfeng1945 <823902826@qq.com>
2017-09-09 23:06:35 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "extends": "standard",
+    env:{
+        "browser": true,
+        "jquery": true
+    },
+    rules:{
+        'space-before-function-paren':'off',
+        'keyword-spacing': 'off',
+        'space-before-blocks': 'off',
+        'space-infix-ops': 'off',
+        'semi':'off',
+        'comma-spacing':'off',
+        'block-spacing': 'off',
+        'padded-blocks': 'off',
+        'no-multiple-empty-lines':'off',
+        'semi-spacing': 'off',
+        'no-trailing-spaces':'off',
+        "indent": 0,
+        "spaced-comment": 'off',
+        "quotes": 'off'
+    }
+};
